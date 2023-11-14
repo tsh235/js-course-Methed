a0ef9554a5e9c367cda0c24177f07a93a7f7fddd
tsh235 <tanyashustrova@yandex.ru>
2023-11-14 13:35:09 +0300

    добавлен второй товар

--- lesson_02/index.js
+++ lesson_02/index.js
@@ -9,3 +9,13 @@
   console.log('Наименование товара: ', productName);
   console.log('Общая стоимость товара: ', productCount * productPrice);
 }
+
+{
+  const productName = 'Дрель';
+  const productCount = '13';
+  const productCategory = 'Инструменты';
+  const productPrice = '5781';
+
+  console.log('Наименование товара: ', productName);
+  console.log('Общая стоимость товара: ', productCount * productPrice);
+}
