eec4695abc49fe7ae9f91bcb4bcc4c44b5d4a545
tsh235 <tanyashustrova@yandex.ru>
2023-11-14 21:14:43 +0300

    модуль 1 урок 3 ДЗ

---
+++ lesson_03/index.js
@@ -0,0 +1,32 @@
+'use strict';
+
+{
+  const productName = 'Шуруповерт';
+  const productCount = '10';
+  const productCategory = 'Инструменты';
+  const productPrice = '2459';
+
+  console.log('Наименование товара: ', productName);
+  console.log('Общая стоимость товара: ', productCount * productPrice);
+}
+
+{
+  const productName = 'Дрель';
+  const productCount = '13';
+  const productCategory = 'Инструменты';
+  const productPrice = '5781';
+
+  console.log('Наименование товара: ', productName);
+  console.log('Общая стоимость товара: ', productCount * productPrice);
+}
+
+{
+  const item = prompt('Введите наименование товара', '');
+  const count = prompt('Введите количество товара', 0);
+  const category = prompt('Введите категорию товара', '');
+  const price = prompt('Введите цену товара', 0);
+  console.log('count: ', typeof count);
+  console.log('price: ', typeof price);
+
+  console.log(`На складе ${+count} шт товара "${item}" на сумму ${+count * +price} руб`);
+}
