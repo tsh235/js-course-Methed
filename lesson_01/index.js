ffca22a097401f04ee46b8168dfeefc6e2311a6a
tsh235 <tanyashustrova@yandex.ru>
2023-11-14 21:20:02 +0300

    ДЗ модуль 1 урок 3

--- lesson_01/index.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const productName = 'Шуруповерт';
-const productCount = '10';
-const productCategory = 'Инструменты';
-const productPrice = '2459';
-
-console.log('Наименование товара: ', productName);
-console.log('Общая стоимость товара: ', productCount * productPrice);
