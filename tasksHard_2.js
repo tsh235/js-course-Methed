7dcfce0c9a3b06ae76e99589210e1978214960d7
tsh235 <tanyashustrova@yandex.ru>
2023-11-15 18:18:38 +0300

    дополнительное ДЗ модуль 1 урок 4

--- tasksHard_2.js
+++ tasksHard_2.js
@@ -1,23 +1,23 @@
 'use strict';
 
-//! Первая задача
+//! Вторая задача
 const income = +prompt('Какой у вас доход?');
 
 if (!Number.isNaN(income) && income > 0) {
   let taxAmount = 0;
 
-  if (income < 15000) {
+  if (income <= 15000) {
     taxAmount = income * 13 / 100;
     console.log(`Сумма налога составляет ${taxAmount}`);
   } 
 
-  if (income >= 15000 && income < 50000) {
-    taxAmount = income * 20 / 100;
+  if (income > 15000 && income <= 50000) {
+    taxAmount = (15000 * 13 / 100) + (income - 15000) * 20 / 100;
     console.log(`Сумма налога составляет ${taxAmount}`);
   } 
 
   if (income >= 50000) {
-    taxAmount = income * 30 / 100;
+    taxAmount = (15000 * 13 / 100) + (50000 - 15000) * 20 / 100 + (income - 50000) * 30 / 100;
     console.log(`Сумма налога составляет ${taxAmount}`);
   } 
   
