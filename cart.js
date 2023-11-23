e0983207ab670a6af8eaa80741407b8ae199773b
tsh235 <tanyashustrova@yandex.ru>
2023-11-23 17:10:09 +0300

    ДЗ модуль 1 урок 12 задание 2 задача 1

--- cart.js
+++ cart.js
@@ -1,55 +1,5 @@
 'use strict';
 
-/*
-Создайте объект cart — корзина
-
-Объект будет содержать следующие свойства:
-  items = пустой массив - это товары
-  totalPrice = 0 - общая стоимость корзины
-  count = 0 - количество товаров
-
-и методы
-  getTotalPrice - получить общую стоимость товаров
-  add - добавить товар
-  increaseCount - увеличить количество товаров
-  calculateItemPrice - посчитать общую стоимость товаров
-  clear - очистить корзину
-  print - распечатать корзину
-
-Описание каждого метода
-
-getTotalPrice()
-метод возвращает значение свойства totalPrice
-
-calculateItemPrice()
-пересчитывает стоимость всей корзины и записывает значение в totalPrice
-
-increaseCount()
-Принимает один параметр(число)
-Увеличивает свойство count на это число
-
-add()
-Принимает три параметра:
-  название товара
-  цену товара
-  количество товара (опциональный параметр, по умолчанию 1 товар)
-
-этот метод формирует объект из полученных параметров
-и добавляет его в свойство items
-так же вызывает все необходимые методы
-чтобы свойства count и totalPrice были актуальные
-
-clear()
-Очищает полностью нашу корзину, возвращает все значения в изначальные
-
-print()
-Выводит в консоль JSON строку из массива items
-и на следующей строке выводит общую стоимость корзины
-
-Для проверки работы функционала добавить 3 или более товаров в корзину
-После вызвать метод print для вывода информации в консоль
-*/
-
 const cart = {
   items: [],
   count: 0,
@@ -85,33 +35,9 @@ const cart = {
   },
 };
 
-const getProduct = () => {
-  const productName = prompt('Введите название товара:');
-
-  let productPrice;
-  do {
-    productPrice = productPrice === undefined ?
-      +prompt('Введите цену товара:') :
-      +prompt('Вы ввели не число! Введите число:');
-  } while (Number.isNaN(productPrice));
-
-  let productCount;
-  do {
-    productCount = productCount === undefined ?
-      +prompt('Введите количество товара:') :
-      +prompt('Вы ввели не число! Введите число:');
-  } while (Number.isNaN(productCount));
-
-  const product = [productName, productPrice, productCount];
-
-  console.log('product: ', product);
-  return product;
-};
-
-for (let i = 0; i < 3; i++) {
-  const [productName, productPrice, productCount] = getProduct();
-  cart.add(productName, productPrice, productCount);
-}
+cart.add('Молоко', '89', '3');
+cart.add('Хлеб', '41', '2');
+cart.add('Масло', '280');
 
 cart.print();
 
