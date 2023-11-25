36441944a193e8895490901efe5e3dad0bc2ac73
tsh235 <tanyashustrova@yandex.ru>
2023-11-25 14:45:45 +0300

    ДЗ модуль 1 урок 13 усложненное 2 внесены изменения по рекомендациям куратора

--- game.js
+++ game.js
@@ -1,8 +1,56 @@
 'use strict';
 
 (() => {
-  const FIGURES_ENG = ['rock', 'scissors', 'paper'];
-  const FIGURES_RUS = ['камень', 'ножницы', 'бумага'];
+  const figuresLang = {
+    ENG: {
+      fig: ['rock', 'scissors', 'paper'],
+      comp: 'Computer',
+      us: 'You',
+      res: 'Result',
+      win: 'You win!',
+      nowin: 'Computer win!',
+      draw: 'Draw in the game!',
+      more: 'Shall we continue?',
+      sure: 'Are you sure you want to get out?',
+      text: 'Ок. The game is over!',
+    },
+    EN: {
+      fig: ['rock', 'scissors', 'paper'],
+      comp: 'Computer',
+      us: 'You',
+      res: 'Result',
+      win: 'You win!',
+      nowin: 'Computer win!',
+      draw: 'Draw in the game!',
+      more: 'Shall we continue?',
+      sure: 'Are you sure you want to get out?',
+      text: 'Ок. The game is over!',
+    },
+    ES: {
+      fig: ['piedra', 'tijeras', 'papel'],
+      comp: 'Computadora',
+      us: 'Usted',
+      res: 'Resultado',
+      win: 'Has ganado!',
+      nowin: 'La computadora ganó!',
+      draw: 'Empate!',
+      more: 'Сontinuamos?',
+      sure: '¿Seguro que quieres salir?',
+      text: 'Picar. ¡Se acabó el juego!',
+    },
+    RUS: {
+      fig: ['камень', 'ножницы', 'бумага'],
+      comp: 'Компьютер',
+      us: 'Вы',
+      res: 'Результат',
+      win: 'Вы выиграли!',
+      nowin: 'Компьютер выиграл',
+      draw: 'Ничья!',
+      more: 'Продолжим?',
+      sure: 'Точно хотите выйти?',
+      text: 'Жаль. Игра окончена!',
+    },
+  };
 
   const getRandomIntInclusive = (min, max) => {
     min = Math.ceil(min);
@@ -10,40 +58,30 @@
     return Math.floor(Math.random() * (max - min + 1) + min);
   };
 
-  const getFigure = (lang, language) => {
+  const getFigure = (lang) => {
     // получили значение от пользователя
-    let figureUser;
+    let figure;
     const getFigurePlayer = () => {
-      figureUser = (prompt([...lang].join(', '))).toLowerCase();
+      figure = (prompt([...lang].join(', '))).toLowerCase();
 
-      if (!lang.some((elem) => elem.startsWith(figureUser))) {
+      if (!lang.some((elem) => elem.startsWith(figure))) {
         getFigurePlayer();
       }
 
-      if (figureUser[0] === 'к' &&
-        (language !== 'EN' || language !== 'ENG')) return 'камень';
-      if (figureUser[0] === 'н' &&
-        (language !== 'EN' || language !== 'ENG')) return 'ножницы';
-      if (figureUser[0] === 'б' &&
-        (language !== 'EN' || language !== 'ENG')) return 'бумага';
-      if (figureUser[0] === 'r' &&
-        (language === 'EN' || language === 'ENG')) return 'rock';
-      if (figureUser[0] === 's' &&
-        (language === 'EN' || language === 'ENG')) return 'scissors';
-      if (figureUser[0] === 'p' &&
-        (language === 'EN' || language === 'ENG')) return 'paper';
+      figure = lang.find((item) => item.includes(figure, 0));
+
+      return figure;
     };
 
-    const figurePlayer = getFigurePlayer();
+    const user = lang.indexOf(getFigurePlayer(), 0);
 
     // получили от компа значение
-    const figureComputer =
-      lang[getRandomIntInclusive(0, lang.length - 1)];
+    const pc = getRandomIntInclusive(0, lang.length - 1);
 
-    console.log(`${figurePlayer[0]} - ${figureComputer[0]}`);
+    console.log(`${lang[pc][0]} - ${lang[user][0]}`);
 
     // возвращаем массив из полученных значений
-    return [figurePlayer, figureComputer];
+    return [user, pc];
   };
 
   const game = (language) => {
@@ -52,89 +90,68 @@
       computer: 0,
     };
 
-    const lang = language === 'EN' || language === 'ENG' ?
-      FIGURES_ENG : FIGURES_RUS;
+    const getFigures = (obj, key = 'RUS') => {
+      if (obj.hasOwnProperty(key)) {
+        return obj[key].fig;
+      }
+    };
+
+    const lang = getFigures(figuresLang, language);
+    console.log('lang: ', lang);
 
     return function start() {
       // проверяем полученный результат
-      const chekResult = ([player, computer], language) => {
+      console.log(figuresLang);
+      const chekResult = ([user, pc]) => {
         if (
-          (player === 'камень' && computer === 'ножницы' ||
-            player === 'rock' && computer === 'scissors') ||
-          (player === 'ножницы' && computer === 'бумага' ||
-            player === 'scissors' && computer === 'paper') ||
-          (player === 'бумага' && computer === 'камень' ||
-            player === 'paper' && computer === 'rock')
+          user === 0 && pc === 1 ||
+          user === 1 && pc === 2 ||
+          user === 2 && pc === 0
         ) {
-          language === 'EN' || language === 'ENG' ?
           alert(`
-          Computer: ${computer}
-          You: ${player}
-          Result: You win!`) :
-          alert(`
-          Компьютер: ${computer}
-          Вы: ${player}
-          Результат: Вы выиграли`);
+          ${figuresLang[language]?.comp ?? 'Компьютер'}: ${lang[pc]}
+          ${figuresLang[language]?.us ?? 'Вы'}: ${lang[user]}
+          ${figuresLang[language]?.res ?? 'Результат'}: ${figuresLang[language]?.win ?? 'Вы победили!'}`);
           result.player++;
-        } else if (player === computer) {
-          language === 'EN' || language === 'ENG' ?
+        } else if (user === pc) {
           alert(`
-          Computer: ${computer}
-          You: ${player}
-          Result: Draw in the game!`) :
-          alert(`
-          Компьютер: ${computer}
-          Вы: ${player}
-          Результат: Ничья`);
+          ${figuresLang[language]?.comp ?? 'Компьютер'}: ${lang[pc]}
+          ${figuresLang[language]?.us ?? 'Вы'}: ${lang[user]}
+          ${figuresLang[language]?.res ?? 'Результат'}: ${figuresLang[language]?.draw ?? 'Ничья!'}`);
         } else {
-          language === 'EN' || language === 'ENG' ?
           alert(`
-          Computer: ${computer}
-          You: ${player}
-          Result: Computer win!`) :
-          alert(`
-          Компьютер: ${computer}
-          Вы: ${player}
-          Результат: Компьютер выиграл`);
+          ${figuresLang[language]?.comp ?? 'Компьютер'}: ${lang[pc]}
+          ${figuresLang[language]?.us ?? 'Вы'}: ${lang[user]}
+          ${figuresLang[language]?.res ?? 'Результат'}: ${figuresLang[language]?.nowin ?? 'Компьютер выиграл!'}`);
           result.computer++;
         }
       };
 
       // продолжение игры
-      const continueGame = (language) => {
-        const maybeMore = language === 'EN' || language === 'ENG' ?
-          confirm('Continue?') : confirm('Продолжим?');
-        if (maybeMore) {
-          const figures = getFigure(lang, language);
-          chekResult(figures, language);
-          continueGame(language);
-        } else {
-          const youSure = language === 'EN' || language === 'ENG' ?
-          confirm('Are you sure you want to finish the game?') :
-          confirm('Точно хотите выйти?');
-          if (!youSure) {
-            const figures = getFigure(lang, language);
-            chekResult(figures, language);
-            continueGame(language);
-          } else {
-            language === 'EN' || language === 'ENG' ?
-              alert(`
-              Ok. The game is over!
-              Your result: ${result.player}
-              Computer result: ${result.computer}
-              See you soon!`) :
-              alert(`
-              Жаль. Игра окончена!
-              Ваш результат: ${result.player}
-              Результат компьютера: ${result.computer}
-              До новых встреч!`);
+      const continueGame = () => {
+        const maybeMore = confirm(`${figuresLang[language]?.more ?? 'Продолжим?'}`);
+        if (!maybeMore) {
+          const youSure = confirm(`${figuresLang[language]?.sure ?? 'Точно хотите выйти?'}`);
+          if (youSure) {
+            alert(`
+            ${figuresLang[language]?.text ?? 'Жаль. Игра окончена!'}
+            ${figuresLang[language]?.us ?? 'Ваш результат'}: ${result.player}
+            ${figuresLang[language]?.comp ?? 'Результат компьютера'}: ${result.computer}`);
+            return;
           }
+
+          const figures = getFigure(lang);
+          chekResult(figures);
+          continueGame();
         }
+        const figures = getFigure(lang);
+        chekResult(figures);
+        continueGame();
       };
 
-      const figures = getFigure(lang, language);
-      chekResult(figures, language);
-      continueGame(language);
+      const figures = getFigure(lang);
+      chekResult(figures);
+      continueGame();
     };
   };
 
